bc782422d1ef958a10dd4935a4a4cd3cd2f4dfce
Shannon Lewis <you@example.com>
2014-03-04 20:54:57 -0800

    updated SJF/SRTF.  Needs to be built to get .class file

--- CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/SJFSchedulingAlgorithm.java
+++ CPUSchedulingSimulator/src/com/jimweller/cpuscheduler/SJFSchedulingAlgorithm.java
@@ -13,43 +13,81 @@ import java.util.*;
 
 public class SJFSchedulingAlgorithm extends BaseSchedulingAlgorithm implements OptionallyPreemptiveSchedulingAlgorithm {
     private boolean preemptive;
+    private Vector<Process> jobs;
+    //active job
 
     SJFSchedulingAlgorithm(){
+    	activeJob = null;
+    	preemptive = false;
+    	jobs = new Vector<Process>();
     }
 
     /** Add the new job to the correct queue.*/
     public void addJob(Process p){
-
+    	Process cur;
+    	if (isPreemptive()) {
+    		for (int i = 0; i < jobs.size(); i++) {
+        		cur = jobs.get(i);
+        		if (p.getInitBurstTime() < cur.getBurstTime()) {	
+        			jobs.add (i, p);
+        		}		//BUGBUG change active job?
+        	}
+    	} else {
+    		for (int i = 0; i < jobs.size(); i++) {
+    			cur = jobs.get(i);
+    			if (p.getInitBurstTime() < cur.getInitBurstTime()) {
+    				job.add (i, p);
+    			}
+    		}
+    	}
+    	
+    	jobs.add(p);
     }
     
     /** Returns true if the job was present and was removed. */
     public boolean removeJob(Process p){
+    	return jobs.remove(p);
     }
 
     /** Transfer all the jobs in the queue of a SchedulingAlgorithm to another, such as
 	when switching to another algorithm in the GUI */
     public void transferJobsTo(SchedulingAlgorithm otherAlg) {
+    	Process cur;
+    	for (int i = jobs.size() - 1; i >= 0; i--) {
+    		cur = jobs.get(i);
+    		otherAlg.addJob(cur);
+    		this.removeJob(cur);
+    	}
     }
 
     /** Returns the next process that should be run by the CPU, null if none available.*/
-    public Process getNextJob(long currentTime){
+    public Process getNextJob(long currentTime) {
+    	Process cur;
+    	for (int i = jobs.size() = 1; i >= 0; i--) {
+    		if (cur.getBurstTime() == 0) {
+    			removeJob(cur);
+    		} else {
+    			break;
+    		}
+    	}
+    	return jobs.get(0);
     }
 
     public String getName(){
-	return "Shortest job first";
+    	return "Shortest job first";
     }
 
     /**
      * @return Value of preemptive.
      */
     public boolean isPreemptive(){
-	return preemptive;
+    	return preemptive;
     }
     
     /**
      * @param v  Value to assign to preemptive.
      */
     public void setPreemptive(boolean  v){
-	preemptive = v;
+    	preemptive = v;
     }
 }
